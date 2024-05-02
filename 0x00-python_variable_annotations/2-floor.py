681eb2e442072e03fe7afb4c680051c4d13d2979
Julius-3367 <korirjulius001@gmail.com>
2024-05-02 00:37:24 -0700

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function floor which takes a float n as argument
+and returns the floor of the float.
+"""
+def floor(n: float) -> int:
+    """Function to return the floor of a float"""
+    return int(n)
+
